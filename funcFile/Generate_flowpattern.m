3ce04833f5cb04eafce26c979411bdf14a24bc76
xxxxlc <913847100@qq.com>
2021-08-04 13:35:14 +0800

    fix

--- funcFile/Generate_flowpattern.m
+++ funcFile/Generate_flowpattern.m
@@ -1,4 +1,4 @@
-function [matrix, feature] = Generate_flowpattern(flowPattern, fileName, GRID_NUM)
+function [matrix, feature] = Generate_flowpattern(flowPattern, fileName, GRID_NUM, Eempty, Efull)
 % This function is used to generate a txt file of a specific flow type 
 % for input to COMSOL
 %
@@ -13,59 +13,67 @@ function [matrix, feature] = Generate_flowpattern(flowPattern, fileName, GRID_NU
 
     addpath('func_flowPattern');
 
-    global Eempty
-    global Efull
+    %% Typical flow pattern
+    if flowPattern ~= 10
+        if flowPattern == 1                                                % Empty_pipe
+            [con, name] = emptyPipe(Eempty, GRID_NUM);
 
-    if flowPattern == 1                                                        % Empty_pipe
-        [con, name] = emptyPipe(Eempty, GRID_NUM);
+        elseif flowPattern == 2                                            % Filled_pipe
+            [con, name] = filledPipe(Eempty, GRID_NUM);
 
-    elseif flowPattern == 2                                                    % Filled_pipe
-        [con, name] = filledPipe(Eempty, GRID_NUM);
+        elseif flowPattern == 3                                            % Laminar_flow
+            [con, name] = laminarFlow(GRID_NUM, Eempty, Efull);
 
-    elseif flowPattern == 3                                                    % Laminar_flow
-        [con, name] = laminarFlow(GRID_NUM, Eempty, Efull);
+        elseif flowPattern == 4                                            % Full_ring
+            [con, name] = fullRing(GRID_NUM, Eempty, Efull);
 
-    elseif flowPattern == 4                                                    % Full_ring
-        [con, name] = fullRing(GRID_NUM, Eempty, Efull);
+        elseif flowPattern == 5                                            % Center_ring
+            [con, name] = centerRing(GRID_NUM, Eempty, Efull);
 
-    elseif flowPattern == 5                                                    % Center_ring
-        [con, name] = centerRing(GRID_NUM, Eempty, Efull);
+        elseif flowPattern == 6                                            % Eccentric_ring
+            [con, name] = eccentricRing(GRID_NUM, Eempty, Efull);
 
-    elseif flowPattern == 6                                                    % Eccentric_ring
-        [con, name] = eccentricRing(GRID_NUM, Eempty, Efull);
+        elseif flowPattern == 7                                            % Center_ring
+            [con, name] = centralFlow(GRID_NUM, Eempty, Efull);
 
-    elseif flowPattern == 7                                                    % Center_ring
-        [con, name] = centralFlow(GRID_NUM, Eempty, Efull);
+        elseif flowPattern == 8                                            % Eccentric_flow
+            [con, name] = eccentricFlow(GRID_NUM, Eempty, Efull);
 
-    elseif flowPattern == 8                                                    % Eccentric_flow
-        [con, name] = eccentricFlow(GRID_NUM, Eempty, Efull);
+        elseif flowPattern == 9                                            % Any_core_stream
+            [con, name] = anyCoreStream(GRID_NUM, Eempty, Efull);
+        end
 
-    elseif flowPattern == 9                                                    % Any_core_stream
-        [con, name] = anyCoreStream(GRID_NUM, Eempty, Efull);
 
-    elseif flowPattern == 10
-        [con, name] = randomFlow(GRID_NUM, Eempty, Efull);
+        showpicture(con);                                                  % Show flow pattern concentration distribution
+
+        flowCoordinateMatrix = Interpolateddata(con);                      % Convert flow pattern concentration distribution 
+                                                                           % matrix to coordinate matrix
+        [FileName, FilePath] = writetxt(flowCoordinateMatrix, filename);   % Write all the information of the coordinate matrix 
+                                                                           % into a txt file
+
+        matrix = con;
+        feature = [name];
+        return;
+    
     end
-
-
-    showpicture(con);
-
-    flowCoordinateMatrix = Interpolateddata(con);
-
+    
+    %% Random_flow_pattern
+    [con_origin, name] = randomFlow(GRID_NUM, Eempty, Efull);              % Random_flow_pattern
+    con_complementary = zeros(GRID_NUM, GRID_NUM) + 3.4 - con_origin + 1;  % Complementary flow pattern of this random flow 
+                                                                           % pattern
+    showpicture(con_origin);
+    showpicture(con_complementary);
+        
+    flowCoordinateMatrix = Interpolateddata(con_origin);                   % Write random flow pattern and its complementary 
+                                                                           % flow pattern into txt file separately
     [FileName, FilePath] = writetxt(flowCoordinateMatrix, filename);
-
-    matrix = con;
+    flowCoordinateMatrix = Interpolateddata(con_complementary);
+    [FileName, FilePath] = writetxt(flowCoordinateMatrix, ...
+                                   strcat(num2str(...
+                                   str2num(fileName(1)) + 1), '.txt'));
+    matrix = {con_origin,con_complementary} ;
     feature = [name];
-
-    if flowPattern == 10
-        con = zeros(GRID_NUM, GRID_NUM) + 3.4 - con + 1;
-        showpicture(con);
-        flowCoordinateMatrix = Interpolateddata(con);
-
-        [FileName, FilePath] = writetxt(flowCoordinateMatrix, ...
-                                        strcat(num2str(...
-                                        str2num(fileName(1)) + 1), '.txt'));
-    end
+    
 
 
 
